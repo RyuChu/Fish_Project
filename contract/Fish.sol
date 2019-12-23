ffeea9247b4a57509e65c0273ec3aaacef384ce3
Leo <shspsleo@gmail.com>
2019-12-23 15:42:38 +0800

    上傳合約，修改檔名

---
+++ contract/Fish.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.11;
+
+contract Fish {
+    // 合約擁有者
+    address private owner;
+    
+    struct UserStruct{
+        string userName;
+        string userPwd;
+        bool isUser;
+    }
+    
+    mapping (address => UserStruct) public userStructs;
+
+    // 事件們，用於通知前端 web3.js
+    event newUserEvent(address indexed from, string name, uint256 timestamp);
+    event deleteUserEvent(address indexed from, string name, uint256 timestamp);
+    event updateUserEvent(address indexed from, string name, uint256 timestamp);
+
+    modifier isOwner() {
+        require(owner == msg.sender, "you are not owner");
+        _;
+    }
+
+    // 建構子
+    constructor() public payable {
+        owner = msg.sender;
+    }
+    
+    // 確認用戶是否已存在 and 登入
+    function isUser(address userAddress) public view returns (bool) {
+        return userStructs[userAddress].isUser;
+    }
+
+    // 註冊新用戶
+    function newUser(string memory userName, string memory userPwd) public returns (bool) {
+        if(isUser(msg.sender)) revert(); 
+        userStructs[msg.sender].userName = userName;
+        userStructs[msg.sender].userPwd = userPwd;
+        userStructs[msg.sender].isUser = true;
+        
+        emit newUserEvent(msg.sender, userName, now);
+        return true;
+    }
+
+    // 刪除用戶
+    function deleteUser() public returns (bool) {
+        if(!isUser(msg.sender)) revert();
+        userStructs[msg.sender].isUser = false;
+        
+        emit deleteUserEvent(msg.sender, userStructs[msg.sender].userName, now);
+        return true;
+    }
+    
+    // 更新用戶資訊
+    function updateUser(string memory userPwd) public returns(bool success) {
+        if(!isUser(msg.sender)) revert();
+        userStructs[msg.sender].userPwd = userPwd;
+        
+        emit updateUserEvent(msg.sender, userStructs[msg.sender].userName, now);
+        return true;
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const Web3 = require('web3');
+
+const web3 = new Web3('http://localhost:8545');
+
+const contract = require('../contract/Fish.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home')
 });
 
 module.exports = router;
